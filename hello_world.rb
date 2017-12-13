84607168d35c7302a7b08817370ad13ecbb5776a
Antonella_Morittu <morittuantonella@gmail.com>
2017-12-13 11:59:39 +0100

    ruby exercises

---
+++ hello_world.rb
@@ -0,0 +1,4 @@
+names = ['Anto', 'Sabi', 'James']
+names.each do |name|
+  puts "Hello #{name}"
+end
---
+++ my_group.rb
@@ -0,0 +1,17 @@
+age = 30
+name = 'Anto'
+gender = 'woman'
+
+puts "#{name} is a #{age} years old #{gender}."
+
+age = 31
+name = 'James'
+gender = 'man'
+
+puts "#{name} is a #{age} years old #{gender}."
+
+age = 32
+name = 'Lars'
+gender = 'man'
+
+puts "#{name} is a #{age} years old #{gender}."
