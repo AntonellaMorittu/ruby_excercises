3bb8bb6aee626bd77a034acb4f6ceb2b7dfbcbd0
Antonella_Morittu <morittuantonella@gmail.com>
2017-12-15 16:13:09 +0100

    99 bottles of beer on the wall

---
+++ beer.rb
@@ -0,0 +1,12 @@
+puts '99 Bottles of beer.'
+
+bottles = 99
+
+while bottles > 1
+  puts "#{bottles} bottles of beer on the wall!"
+  bottles -= 1
+end
+
+puts '1 bottle of beer on the wall..'
+
+puts "It's time to go home!"
