02cb442c03111eb7258d9e2688f8ff91e65f27b2
Antonella_Morittu <morittuantonella@gmail.com>
2017-12-21 14:03:10 +0100

    changed names on group

--- my_group.rb
+++ my_group.rb
@@ -1,17 +1,17 @@
 age = 30
-name = 'Anto'
+name = 'person_1'
 gender = 'woman'
 
 puts "#{name} is a #{age} years old #{gender}."
 
 age = 31
-name = 'James'
+name = 'person_2'
 gender = 'man'
 
 puts "#{name} is a #{age} years old #{gender}."
 
 age = 32
-name = 'Lars'
+name = 'person_3'
 gender = 'man'
 
 puts "#{name} is a #{age} years old #{gender}."
